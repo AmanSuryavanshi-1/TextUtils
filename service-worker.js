dd4e3caba81b188e5a6b7eac8d84372d19d01392
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2022-09-18 22:50:59 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/AmanSuryavanshi-1/TextUtils/precache-manifest.9b0f13888508d05dde77c527cc24fff9.js"
+  "/AmanSuryavanshi-1/TextUtils.git/precache-manifest.9a9f094d6c32d5c4cc97789f612d9498.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/AmanSuryavanshi-1/TextUtils/index.html", {
+workbox.routing.registerNavigationRoute("/AmanSuryavanshi-1/TextUtils.git/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
