e01388f8801bbc68285d35e7c656f1a24c0f9f2f
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2022-09-18 22:31:59 +0530

    Updates

---
+++ precache-manifest.9b0f13888508d05dde77c527cc24fff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8be386e4ddf87027bbe5",
+    "url": "/AmanSuryavanshi-1/TextUtils/static/css/main.c80f5e7e.chunk.css"
+  },
+  {
+    "revision": "8be386e4ddf87027bbe5",
+    "url": "/AmanSuryavanshi-1/TextUtils/static/js/main.8be386e4.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/AmanSuryavanshi-1/TextUtils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fa125e7b71c2341de334",
+    "url": "/AmanSuryavanshi-1/TextUtils/static/js/2.fa125e7b.chunk.js"
+  },
+  {
+    "revision": "cadc1912662f497b522f",
+    "url": "/AmanSuryavanshi-1/TextUtils/static/js/runtime~main.cadc1912.js"
+  },
+  {
+    "revision": "c0a488c3115b00469ec0cdaee32a380f",
+    "url": "/AmanSuryavanshi-1/TextUtils/index.html"
+  }
+];
